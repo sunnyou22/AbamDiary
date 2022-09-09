bd57c76fea7b300806f8a8515459bf1159ab1e97
sunnyou22 <sunnyouyaya22@gmail.com>
2022-09-09 22:39:49 +0900

    Initial Commit

---
+++ AbamDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AbamDiary
+//
+//  Created by 방선우 on 2022/09/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
