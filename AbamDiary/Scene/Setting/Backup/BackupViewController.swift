5d781c72c049045dbd5190115181b1a77fb3a6db
sunnyou22 <sunnyouyaya22@gmail.com>
2022-10-01 13:16:21 +0900

    백업복구 테이블뷰 디퍼블로 셀 기본 만들기

--- AbamDiary/Scene/Setting/Backup/BackupViewController.swift
+++ AbamDiary/Scene/Setting/Backup/BackupViewController.swift
@@ -20,10 +20,13 @@ enum Section: CaseIterable {
 class BackupViewController: BaseViewController {
     
     var backupView = BackupView()
-    var hashableList: [String]?
+    var hashableList: [String] = []
+    
     lazy var arr: [Id] = {
         return self.hashableList.map { Id(name: $0) }
     }()
+    
+    var dataSource: UITableViewDiffableDataSource<Section, Id>!
     
     override func loadView() {
         view = backupView
@@ -31,7 +34,32 @@ class BackupViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        backupView.tableView.delegate = self
-//        backupView.
+        backupView.backupFileButton.addTarget(self, action: #selector(clickedBackupButton), for: .touchUpInside)
+       setDataSource()
+    }
+    
+    @objc func clickedBackupButton() {
+        setTextData(text: backupView.backupFileNameTextField.text ?? "")
+    }
+    
+    // 스냅샷에서 데이터를 뽑아와서 셀에 보여지는 것
+    func setDataSource() {
+        self.dataSource = UITableViewDiffableDataSource<Section, Id>(tableView: backupView.tableView, cellProvider: { tableView, indexPath, itemIdentifier in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingDefaultTableViewCell.reuseIdentifier, for: indexPath) as? SettingDefaultTableViewCell else {
+                preconditionFailure()
+            }
+            cell.subTitle.text = itemIdentifier.name
+            
+            return cell
+        })
+    }
+    
+    //셀에 들어갈 데이터 즉 스냅샷
+    func setTextData(text: String) {
+        let contents = [Id(name: text)]
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Id>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(contents)
+        self.dataSource.apply(snapshot, animatingDifferences: true)
     }
 }
